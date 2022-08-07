69e3550603a0de61ef3cba6eb7dc8115f23a7f6b
KBowei <97340226+KBowei@users.noreply.github.com>
2022-08-07 23:17:37 +0800

    将打印编解时间更换为传参，打印在test.py中调用

--- coder.py
+++ coder.py
@@ -1,7 +1,7 @@
 import os, time
 import numpy as np
 import torch
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class Coder():
     """在model.encode和decode的基础上封装了时间测试模块等"""
@@ -14,11 +14,12 @@ class Coder():
         start_time = time.time()
         # 因为ImageCoder部分和Model的结构相关，应该将ImageCoder负责的功能转移到model内部
         self.model.encode(x,self.filename,postfix)
-        print('Encode Time:\t', round(time.time() - start_time, 3), 's')
+        enc_time = round(time.time() - start_time, 3)
+        return enc_time
 
     @torch.no_grad()
     def decode(self, postfix=''):
         start_time = time.time()
         out = self.model.decode(self.filename,postfix=postfix)
-        print('Decode Time:\t', round(time.time() - start_time, 3), 's')
-        return out
+        dec_time = round(time.time() - start_time, 3)
+        return out, dec_time
